5a41834e37bd61c854dcaaaf9dabe843b530e82f
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2022-10-23 21:42:55 +0700

    Time: 80 ms (68.39%), Space: 14.2 MB (15.31%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,27 +1,29 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp = {}
+        store = {}
         
         def dfs(i,j):
-            if (i,j) in dp:
-                return dp[(i,j)]
+            if (i,j) in store:
+                return store[(i,j)]
             
             if i >= len(s) and j >= len(p):
                 return True
             
-            if i <= len(s) and j >= len(p):
+            if j >= len(p):
                 return False
             
             match = i < len(s) and ((s[i] == p[j]) or (p[j] == '.'))
             
-            if j < len(p)-1 and p[j+1] == '*':
-                dp[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
-                return dp[(i,j)]
+            if j + 1 < len(p) and p[j+1] == '*':
+                store[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
+                return store[(i,j)]
             
             if match:
-                dp[(i,j)] = dfs(i+1,j+1) 
-                return dp[(i,j)]
+                store[(i,j)] = dfs(i+1,j+1) 
+                return store[(i,j)]
             
+            return False
+        
         return dfs(0,0)
                       
                 
