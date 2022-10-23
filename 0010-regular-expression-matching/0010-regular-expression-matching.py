9166402cd619983602a90636a826fa1babe5cc9e
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2022-10-23 21:43:50 +0700

    Time: 72 ms (76.40%), Space: 14.2 MB (28.44%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -12,7 +12,7 @@ class Solution:
             if j >= len(p):
                 return False
             
-            match = i < len(s) and ((s[i] == p[j]) or (p[j] == '.'))
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
             
             if j + 1 < len(p) and p[j+1] == '*':
                 store[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
