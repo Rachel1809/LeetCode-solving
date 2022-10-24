3bdb43f2cadc176a4dd77ee62df0571f1adcd426
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2022-10-24 20:07:59 +0700

    Time: 48 ms (79.40%), Space: 13.8 MB (74.53%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -2,24 +2,24 @@ class Solution:
     def isNumber(self, s: str) -> bool:
         num, sign, exp, dec = False, False, False, False
 
-        for char in s:
-            if '0' <= char <= '9':
+        for i in range(len(s)):
+            if '0' <= s[i] <= '9':
                 num = True
 
-            elif char in '+-':
+            elif s[i] in '+-':
                 if num or sign or dec:
                     return False
                 else:
                     sign = True
 
-            elif char in 'eE':
+            elif s[i] in 'eE':
                 if not num or exp:
                     return False
                 else:
                     exp = True
                     sign, num, dec = False, False , False
 
-            elif char == '.':
+            elif s[i] == '.':
                 if dec or exp:
                     return False
                 else:
