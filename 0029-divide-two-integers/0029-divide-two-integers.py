4848e0cf1198d0452cc63e06f1f6b50d4f2bda69
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-07 15:44:40 +0700

    Time: 31 ms (88.23%), Space: 13.8 MB (66.70%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,6 +1,5 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        q = 0
         sign = (dividend < 0) ^ (divisor < 0)
         dividend, divisor = abs(dividend), abs(divisor)
         q = len(range(0, dividend-divisor+1, divisor))
