ddfad3542ebed28ca6dd22026d44f8529eba7a4e
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 22:59:59 +0700

    Time: 60 ms (81.04%), Space: 14.1 MB (77.24%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -3,15 +3,15 @@ class Solution:
         def backtrack(comb, counter, res):
             if len(comb) == len(nums):
                 res.append(comb[:])
-            else:
-                for num in counter:
-                    if counter[num] > 0:
-                        # add this number into the current combination
-                        counter[num] -= 1
-                        # continue the exploration
-                        backtrack(comb + [num], counter, res)
-                        # revert the choice for the next exploration
-                        counter[num] += 1
+                return res
+            for num in counter:
+                if counter[num] > 0:
+                    # add this number into the current combination
+                    counter[num] -= 1
+                    # continue the exploration
+                    backtrack(comb + [num], counter, res)
+                    # revert the choice for the next exploration
+                    counter[num] += 1
             return res
 
         return backtrack([], Counter(nums), [])
