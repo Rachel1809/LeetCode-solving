55410a73ce861c1a627dd8a686e5d3247232f2b6
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 22:48:15 +0700

    Time: 64 ms (69.26%), Space: 14.2 MB (62.30%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,20 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        def backtrack(comb, counter):
+        def backtrack(comb, counter, res):
             if len(comb) == len(nums):
-                results.append(comb[:])
-                return
+                res.append(comb[:])
+            else:
+                for num in counter:
+                    if counter[num] > 0:
+                        # add this number into the current combination
+                        counter[num] -= 1
+                        # continue the exploration
+                        backtrack(comb + [num], counter, res)
+                        # revert the choice for the next exploration
+                        counter[num] += 1
+            return res
 
-            for num in counter:
-                if counter[num] > 0:
-                    # add this number into the current combination
-                    counter[num] -= 1
-                    # continue the exploration
-                    backtrack(comb + [num], counter)
-                    # revert the choice for the next exploration
-                    counter[num] += 1
-
-        backtrack([], Counter(nums))
-
-        return results
+        return backtrack([], Counter(nums), [])
