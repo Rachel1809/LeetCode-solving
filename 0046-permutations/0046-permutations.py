dc51f52c95540c6d536f594b6f95f88483da3762
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 22:19:40 +0700

    Time: 36 ms (92.38%), Space: 14.1 MB (52.84%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,11 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:        
-        res = []
-
         def permu(remain, tmp, res):
             if not remain:
                 res.append(tmp[:])
             else:
                 for i in range(len(remain)):
                     permu(remain[:i] + remain[i+1:], tmp + [remain[i]], res)
+            return res
 
-        permu(nums, [], res)
-        return res
+        return permu(nums, [], [])
