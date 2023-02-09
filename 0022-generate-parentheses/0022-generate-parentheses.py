cda3e02c4833f63e3f6f9b38222df4ef32e77589
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 23:33:44 +0700

    Time: 39 ms (54.83%), Space: 14 MB (97.42%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,16 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def dfs(left, right, s):
+        def dfs(left, right, s, res):
             if len(s) == n * 2:
                 res.append(s)
-                return 
+                return res
 
             if left < n:
-                dfs(left + 1, right, s + '(')
+                dfs(left + 1, right, s + '(', res)
 
             if right < left:
-                dfs(left, right + 1, s + ')')
+                dfs(left, right + 1, s + ')', res)
+            return res
 
-        res = []
-        dfs(0, 0, '')
-        return res
+        return dfs(0, 0, '', [])
