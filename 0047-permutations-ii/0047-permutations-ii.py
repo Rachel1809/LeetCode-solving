6c8c8ffe39a990fa1b44fd594ff19e66aef123e9
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 22:36:17 +0700

    Time: 2412 ms (5.01%), Space: 14.2 MB (77.24%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = set()
+        res = []
 
         def permu(remain, tmp, res):
-            if not remain:
-                res.add(tuple(tmp))
-            else:
-                for i in range(len(remain)):
-                    permu(remain[:i] + remain[i+1:], tmp + [remain[i]], res)
-            return list(res)
+            if tmp not in res:
+                if not remain:
+                    res.append(tmp[:])
+                else:
+                    for i in range(len(remain)):
+                        permu(remain[:i] + remain[i+1:], tmp + [remain[i]], res)
+                    
+            return res
         return permu(nums, [], res)
