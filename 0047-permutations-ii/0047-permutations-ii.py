7832b267a56365231be21a93dae6596833ded448
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-09 22:46:14 +0700

    Time: 52 ms (96.99%), Space: 14.4 MB (23.93%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -3,20 +3,16 @@ class Solution:
         results = []
         def backtrack(comb, counter):
             if len(comb) == len(nums):
-                # make a deep copy of the resulting permutation,
-                # since the permutation would be backtracked later.
-                results.append(list(comb))
+                results.append(comb[:])
                 return
 
             for num in counter:
                 if counter[num] > 0:
                     # add this number into the current combination
-                    comb.append(num)
                     counter[num] -= 1
                     # continue the exploration
-                    backtrack(comb, counter)
+                    backtrack(comb + [num], counter)
                     # revert the choice for the next exploration
-                    comb.pop()
                     counter[num] += 1
 
         backtrack([], Counter(nums))
