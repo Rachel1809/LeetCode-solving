7b67031418d77484933f02d2673ffbc33ffa6fca
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-13 22:04:45 +0700

    Time: 70 ms (66.38%), Space: 13.9 MB (79.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,18 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if (l1 == None):
+        if l1 == None:
             return l2
-        if (l2 == None):
+        if l2 == None:
             return l1
         
-        x = l1.val + l2.val
-        output = ListNode(x%10)
-        output_tail = output
-        x //= 10
-        l1 = l1.next
-        l2 = l2.next
-        
+        #x = l1.val + l2.val
+        #output = ListNode(x%10)
+        #output_tail = output
+        #x //= 10
+        #l1 = l1.next
+        #l2 = l2.next
+        output = None
+        x = 0
         while (l1 != None or l2 != None):
             if (l1 != None):
                 x += l1.val
@@ -24,10 +25,16 @@ class Solution:
             if (l2 != None):
                 x += l2.val
                 l2 = l2.next
-            output_tail.next = ListNode(x%10)
+                
+            if not output:
+                output = ListNode(x%10)
+                output_tail = output
+            else:
+                output_tail.next = ListNode(x%10)
+                output_tail = output_tail.next
+
             x //= 10
                 
-            output_tail = output_tail.next
             
         output_tail.next = ListNode(x) if x != 0 else None
         return output
