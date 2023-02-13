a0fdac072c338416b1da3d23b2b1b217be752cb0
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-13 21:30:27 +0700

    Time: 36 ms (75.00%), Space: 13.8 MB (63.70%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,10 +1,22 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         res = -1
+        d = {
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "0": 0,
+        }
         for i in num1, num2:
             tmp = 0
-            for j in range(len(i)):
-                tmp = (ord(i[j]) - 48) + tmp * 10
+            for j in i:
+                tmp = d[j] + tmp * 10
                 
             if res == -1:
                 res = tmp
