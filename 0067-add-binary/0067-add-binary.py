802510a03efeb33bdc2071f5b19a1d29d78ff01a
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-14 09:56:57 +0700

    Time: 47 ms (20.07%), Space: 13.9 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,18 +4,14 @@ class Solution:
         j = len(b) - 1
         res = ""
         odd = 0
-        while i >= 0 and j >= 0:
-            res += str((odd + int(a[i]) + int(b[j])) % 2)
-            odd = (odd + int(a[i]) + int(b[j])) // 2
+        while i >= 0 or j >= 0:
+            if i >= 0:
+                odd += int(a[i])
+            if j >= 0:
+                odd += int(b[j])
+            res += str(odd % 2)
+            odd //= 2
             i -= 1
-            j -= 1
-        while i >= 0:
-            res += str((odd + int(a[i])) % 2)
-            odd = (odd + int(a[i])) // 2
-            i -= 1
-        while j >= 0:
-            res += str((odd + int(b[j])) % 2)
-            odd = (odd + int(b[j])) // 2
             j -= 1
         if odd > 0:
             res += "1"
