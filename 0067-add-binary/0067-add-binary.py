1df7b27b4f2bfb49a7aa91094343e1d7d5c6b4f3
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-14 10:10:49 +0700

    Time: 38 ms (53.43%), Space: 13.9 MB (18.45%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,32 +1,14 @@
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        #i = len(a) - 1
-        #j = len(b) - 1
-        #res = ""
-        #odd = 0
-        #while i >= 0 or j >= 0:
-        #    if i >= 0:
-        #        odd += int(a[i])
-        #    if j >= 0:
-        #        odd += int(b[j])
-        #    res += str(odd % 2)
-        #    odd //= 2
-        #    i -= 1
-        #    j -= 1
-        #if odd > 0:
-        #    res += "1"
-
-        #return res[::-1]
-        
+    def addBinary(self, a: str, b: str) -> str:        
         tmp = int(a) + int(b)
-        odd, res = 0, ""
         
         if tmp == 0:
             return "0"
         
+        odd, res = 0, ""
+
         while (tmp > 0):
-            remain = ((tmp % 10) + odd) % 2
-            res += str(remain)
+            res += str(((tmp % 10) + odd) % 2)
             odd = ((tmp % 10) + odd) // 2
             tmp //= 10
             
