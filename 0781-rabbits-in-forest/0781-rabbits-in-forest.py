bf60be287af913bea0888cba1142bd158c7aa9e9
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-21 13:42:56 +0700

    Time: 39 ms (90.83%), Space: 14.1 MB (24.58%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.py
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -4,6 +4,6 @@ class Solution:
         res = 0
         for i, j in x.items():
             tmp = i + 1
-            res += -(-j//tmp) * tmp
-            
+            #res += -(-j//tmp) * tmp
+            res += ceil(j/tmp) * tmp
         return res
