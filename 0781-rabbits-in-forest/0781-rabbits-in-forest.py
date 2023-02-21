67c84ebb007ed6541055aa4520dddd6e6ef3f588
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-02-21 13:40:47 +0700

    Time: 49 ms (42.08%), Space: 13.8 MB (95.83%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        x = Counter(answers)
+        res = 0
+        for i, j in x.items():
+            tmp = i + 1
+            res += -(-j//tmp) * tmp
+            
+        return res
