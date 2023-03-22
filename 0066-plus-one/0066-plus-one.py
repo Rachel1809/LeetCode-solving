c21713bea0a9fb039b08ef1ce9b250deeac8154c
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-03-22 14:53:33 +0700

    Time: 42 ms (15.06%), Space: 13.9 MB (6.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,15 +2,15 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         odd = 1
         i = len(digits) - 1
-        while i >= 0:
+        while odd > 0:
+            if i < 0:
+                digits.insert(0, odd)
+                break
             tmp = digits[i]
             digits[i] = (tmp + odd) % 10
             odd = (tmp + odd) // 10
-            if odd == 0:
-                break
             i -= 1
+
             
-        if odd > 0:
-            digits.insert(0, odd)
         return digits
                 
