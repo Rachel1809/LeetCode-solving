a1169c587c0f4a8a0824b508cb579a4a704c503f
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-03-22 14:20:57 +0700

    Time: 35 ms (62.24%), Space: 13.8 MB (56.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,9 +3,8 @@ class Solution:
         res = []
         for i in range(numRows):
             tmp = [1]*(i+1)
-            if i > 1:
-                for j in range(1, i):
-                    tmp[j] = res[i-1][j] + res[i-1][j-1]
+            for j in range(1, i):
+                tmp[j] = res[i-1][j] + res[i-1][j-1]
             res.append(tmp)
             
         return res
