5186fb239bb84104104dcc9cd928dbf2837f4ce9
Rachel1809 <91553670+Rachel1809@users.noreply.github.com>
2023-04-08 22:57:08 +0700

    Time: 51 ms (8.03%), Space: 14.3 MB (27.50%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,15 +1,32 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def dfs(left, right, s, res):
-            if len(s) == n * 2:
-                res.append(s)
-                return res
-
-            if left < n:
-                dfs(left + 1, right, s + '(', res)
-
-            if right < left:
-                dfs(left, right + 1, s + ')', res)
-            return res
-
-        return dfs(0, 0, '', [])
+        
+        current_open, current_close = 0, 0
+        
+        ans = []
+        
+        cur_sol = []
+        
+        def choose(i):
+            nonlocal current_open, current_close
+            if i == 2*n:
+                if current_open == current_close:
+                    ans.append("".join(cur_sol))
+                return
+            
+            # choose the closing parenthesis
+            if current_open > current_close:
+                cur_sol.append(")")
+                current_close += 1
+                choose(i+1)
+                current_close -= 1
+                cur_sol.pop()
+                
+            cur_sol.append("(")
+            current_open += 1
+            choose(i+1)
+            current_open -= 1
+            cur_sol.pop()
+            
+        choose(0)
+        return ans
